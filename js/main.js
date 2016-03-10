082c62f8bfbc6dd776c22c526f617e823e79f95b
johnhyde <johnhydefour@gmail.com>
2016-03-10 10:56:32 -0800

    help

--- js/main.js
+++ js/main.js
@@ -15,7 +15,26 @@ var animation = {
 	height
 }
 var frame = {
+	src,
 	image,
 	width,
 	height
+}
+function copyJson(json) {
+	return JSON.parse(JSON.stringify(json));
+}
+function newFrame(src) {
+	var newFrame = copyJson(frame);
+	newFrame.src = src;
+	newFrame.image = $('<img src=' + src + '>');
+	newFrame.image.onload = function() {
+		newFrame.width = newFrame.image.getWidth();
+		newFrame.height = newFrame.image.getHeight();
+	}
+	return newFrame;
+}
+function newAnimation(srcs) {
+	for (var i = 0; i < srcs.length; i++) {
+		frames.push(newFrame(srcs[i]));
+	}
 }
